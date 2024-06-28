bbfb8807d49981e730dd04858e5dbefc1707c06e
Rasikapriya MV. <rasikapriya.mv@optisolbusiness.com>
2024-06-28 10:16:54 +0530

    fourth push

---
+++ Object Repository/Redbus/Page_Bus Ticket Booking Online made Easy, S_c30423/a_View All.rs
@@ -0,0 +1,146 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_View All</name>
+   <tag></tag>
+   <elementGuidId>154b3d52-553f-4b3c-858f-0145c9a72601</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div:nth-of-type(2) > a.OfferSection__ViewAllText-sc-16xojcc-1.eVcjqm</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='homeV2-root']/div[3]/div/div[2]/a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>div >> internal:has-text=/^Government Buses View All$/ >> internal:role=link</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>98642173-7383-48ff-850c-9311daf9ea41</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://www.redbus.in/online-booking/rtc-directory</value>
+      <webElementGuid>e5654ddc-5097-41ad-90fc-ad0ed8e57a62</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>target</name>
+      <type>Main</type>
+      <value>_blank</value>
+      <webElementGuid>f0f6bb4c-0b34-4f5e-88c1-a76ab4858362</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>OfferSection__ViewAllText-sc-16xojcc-1 eVcjqm</value>
+      <webElementGuid>f0853fe6-c4c6-40c5-b909-0eed4b436386</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>View All</value>
+      <webElementGuid>7c6c1c62-dc0d-48f0-9b86-6480dd67c7f1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;homeV2-root&quot;)/div[@class=&quot;rtcSection&quot;]/div[@class=&quot;rtcHeadViewAll&quot;]/div[2]/a[@class=&quot;OfferSection__ViewAllText-sc-16xojcc-1 eVcjqm&quot;]</value>
+      <webElementGuid>fb7e4d5f-c1cb-433f-bfe1-ab61b0dd94ae</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='homeV2-root']/div[3]/div/div[2]/a</value>
+      <webElementGuid>1b33b9da-caa7-461b-82a5-cdea816ad97e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>(//a[contains(text(),'View All')])[2]</value>
+      <webElementGuid>39989fb2-058c-4a1b-9602-ab71300477db</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Government Buses'])[1]/following::a[1]</value>
+      <webElementGuid>d0ad1921-4133-43f0-b1df-3503dcb06bed</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='SBNEW'])[1]/following::a[1]</value>
+      <webElementGuid>08cd99f8-bf8f-437c-86c3-381077f8df6d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='APSRTC'])[1]/preceding::a[1]</value>
+      <webElementGuid>0c2d0d9d-fd6b-4b37-baa5-687f5c27e31f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='ఆంధ్రప్రదేశ్ రాష్ట్ర రోడ్డు రవాణా సంస్థ'])[1]/preceding::a[1]</value>
+      <webElementGuid>a55eab91-f6a1-4e68-890c-f04d088d3026</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, 'https://www.redbus.in/online-booking/rtc-directory')]</value>
+      <webElementGuid>8f083e10-555c-4f39-9665-435aea2154bb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/a</value>
+      <webElementGuid>de789128-f814-4bf3-aaaa-b08f087feb43</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'https://www.redbus.in/online-booking/rtc-directory' and (text() = 'View All' or . = 'View All')]</value>
+      <webElementGuid>bf7ea55c-cd45-40f2-87cf-5e01699d763a</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
